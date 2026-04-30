f9b7eeb1195045c51d3e09c9185fe3989d4b5ec6
Tham Le <tham@kyber.media>
2026-04-30 19:40:46 +0200

    Resolve hostname to IP with getaddrinfo
    Before we can call sendto we need a sockaddr_in. getaddrinfo handles
    both hostnames and dotted-decimal strings uniformly.

--- traceroute.c
+++ traceroute.c
@@ -9,9 +9,29 @@ static int create_icmp_socket(void) {
     return sockfd;
 }
 
+static int resolve_target(const char *target, struct sockaddr_in *dest) {
+    struct addrinfo hints, *res;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = AF_INET;
+    hints.ai_socktype = SOCK_RAW;
+    hints.ai_protocol = IPPROTO_ICMP;
+
+    int ret = getaddrinfo(target, NULL, &hints, &res);
+    if (ret != 0) {
+        fprintf(stderr, "ft_traceroute: %s: %s\n", target, gai_strerror(ret));
+        return -1;
+    }
+    *dest = *(struct sockaddr_in *)res->ai_addr;
+    freeaddrinfo(res);
+    return 0;
+}
+
 int traceroute(char *target, struct s_options *opts) {
-    (void)target;
     (void)opts;
+
+    struct sockaddr_in dest;
+    if (resolve_target(target, &dest) < 0)
+        return 1;
 
     int sockfd = create_icmp_socket();
     if (sockfd < 0)
