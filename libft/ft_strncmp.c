05bbcdcd3f3afe9013833fd79f5204e982dedaa0
Tham Le <tham@kyber.media>
2026-06-06 02:03:32 +0200

    traceroute: remove accidental Kyber license headers

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,23 +1,3 @@
-// Project Kyber: ft_strncmp.c
-// Copyright © 2022-2026 Kyber SAS
-// SPDX-License-Identifier: LicenseRef-Kyber-Commercial OR AGPL-3.0-or-later
-//
-// This file is both under dual license: AGPLv3 and a Commercial one.
-//
-// ----
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 #include "libft.h"
 
 int ft_strncmp(const char *s1, const char *s2, size_t n) {
